7f055b708969f1d2639b1260dbec15a7672d0625
varun97531 <varun97531@github.com>
2019-09-14 16:15:07 +0530

    Adding new lines of code

--- check.py
+++ check.py
@@ -1,1 +1,6 @@
 print("Checking the git version control system")
+res = input("Are you liking git version control system : ")
+if(res == 'Y' or res == 'y'):
+    print("Well done")
+else:
+    print("Try it harder")
